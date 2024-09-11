9e605a7a0e6e8aff8932a8a4b32c6203b065eb15
021.898695 <021.898695@C305-SFT-012>
2024-09-11 14:56:13 -0300

    Adicionar arquivos de projeto.

---
+++ FeaASP/Models/contato.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FEAasp.Models
+{
+    public class contato
+    {
+        public int idContato { get; set; }
+        public string nomeContato { get; set; }
+        public string emailcontato { get; set; }
+
+        public int telefoneContato { get; set; }    
+        public string cidade { get; set; }
+        public string estado { get; set; } 
+        
+        public string mensagem. { get; set;}
+    }
+}
